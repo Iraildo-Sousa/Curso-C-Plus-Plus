f1fa1a59541c8b5716d6746dde878a0261f896f4
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2024-11-13 01:12:09 -0300

    Create Codigo1_Testando.cpp
    Esse é um código teste para iniciar os estudos de c++.

---
+++ Capitulo1/Codigo1_Testando.cpp
@@ -0,0 +1,22 @@
+//Código teste do nosso primeiro programa em C++
+#include <iostream>
+
+int main ()
+{
+     std::cout << "Bem vindo à C++!\n";
+
+    return 0;//indica que o programa terminou com sucesso
+
+}
+
+/*Sequencia de escape:
+
+\n - Nova linha.
+\t - Tabulação horizontal. Move o cursor para próxima tabulação.
+\r - Retorno do carro. Posiciona o cursor da tela no início da linha atual: não avança para a próxima linha.
+
+\a - Alerta. Faz soar o alarme do sistema.
+\\ - Barra invertida. Utilizada para imprimir um caractere barra invertida.
+\" - Aspas. Imprimir aspas.
+
+*/
