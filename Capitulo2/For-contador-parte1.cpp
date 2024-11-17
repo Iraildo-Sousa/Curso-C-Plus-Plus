6667397255f47f2f9e093c128895d6529c5e8077
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2024-11-17 11:31:11 -0300

    Create For-contador-parte1.cpp
    Primeiro teste utilizando a estrutura de repetição For e testando um contador.

---
+++ Capitulo2/For-contador-parte1.cpp
@@ -0,0 +1,17 @@
+//Programa destinado aos primeiros testes de for em c++.
+
+#include <iostream>
+
+using std :: cout;
+using std :: endl;
+
+int main ()
+{
+
+    for (int contador =1; contador <= 10; contador++)
+        cout << contador << endl;
+    
+
+    return 0;
+
+}//int main ()
