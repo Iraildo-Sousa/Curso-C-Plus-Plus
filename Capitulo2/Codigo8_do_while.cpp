75150197b21fff95bba0384e07d03d0976ba9bb5
Nodelay <121840258+Iraildo-Sousa@users.noreply.github.com>
2024-11-19 22:03:36 -0300

    Create Codigo8_do_while.cpp
    Um treinamento com Do While.

---
+++ Capitulo2/Codigo8_do_while.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using std :: cout;
+using std :: endl;
+
+int main ()
+{
+
+    int contador = 0;
+
+    do {
+
+        cout << contador << " ";
+
+    }//do
+
+    while (++contador <= 10);
+
+    cout << endl;
+
+    return 0;
+
+}//int main ()
